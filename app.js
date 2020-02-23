85ad8ceaffa78e0ab54080d04d188f4c9077ff62
Mohd Faisal <mohd@thetrustsearch.com>
2020-02-23 13:51:15 +0530

    add template structures

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(3000);
